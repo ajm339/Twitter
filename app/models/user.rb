c0f8c8f6c632e9e87f88ccd07470d2591cc53d79
Alex Meyers <ajm339@cornell.edu>
2013-12-01 15:04:39 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-    has_secure_password
     #initialize
-    before_save { email.downcase! }
+    before_save { self.email = email.downcase }
+    before_create :create_remember_token
     #name
     validates :name, presence: true, length:{ maximum:50}
     #email
@@ -9,5 +9,21 @@ class User < ActiveRecord::Base
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
     #password
+    has_secure_password
     validates :password, length: { minimum: 6 }
+    
+    
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+    
+    def User.encrypt(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
+    
+    private
+    
+    def create_remember_token
+        self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
